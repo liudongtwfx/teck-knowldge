63497233942b7610659c331ed1f585f48b221d1f
Liu Dong <liudong17@meituan.com>
2020-03-29 15:22:25 +0800

    [MOD] gc

--- src/main/java/jl/gc/GcExample.java
+++ src/main/java/jl/gc/GcExample.java
@@ -4,11 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.lang.ref.SoftReference;
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 
+/**
+ * https://www.jianshu.com/p/74d126dd5544
+ */
 public class GcExample {
 
     private static final String JVM_ARGS = "-Xms64M\n" +
@@ -17,14 +23,17 @@ public class GcExample {
             "-verbose:gc\n" +
             "-XX:+PrintHeapAtGC\n" +
             "-XX:SurvivorRatio=8\n" +
-            "-XX:+UseConcMarkSweepGC";
+            "-XX:+UseConcMarkSweepGC\n" +
+            "-XX:+PrintTenuringDistribution";//对象晋升的日志
 
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             //functionWithStack();
             // List<LargeExample> largeExamples = functionWithReturn();
         }
-        demoWithWeakReferenced();
+        Map<Integer, WeakReference<BigArray>> integerWeakReferenceMap = demoWithWeakReferenced();
+        integerWeakReferenceMap.forEach((k, v) -> System.out.println(k + " " + v.get()));
+        //demoWithSoftReferenced();
     }
 
     private static void functionWithStack() {
@@ -44,10 +53,25 @@ public class GcExample {
 
     /**
      * 一般在弱引用的同时，这个对象可能也被强引用了。如果这个强引用消失了，系统就开始回收弱引用。
+     *
+     * @return
      */
-    private static void demoWithWeakReferenced() {
+    private static Map<Integer, WeakReference<BigArray>> demoWithWeakReferenced() {
+        Map<Integer, WeakReference<BigArray>> bigArrayWeakReferenceMap = new HashMap<>();
         for (int i = 0; i < 100; i++) {
-            WeakReference<BigArray> bigArrayWeakReference = new WeakReference<>(new BigArray(i));
+            bigArrayWeakReferenceMap.put(i, new WeakReference<>(new BigArray(i)));
+        }
+        return bigArrayWeakReferenceMap;
+    }
+
+    /**
+     * 内存不够的时候回收
+     */
+    private static void demoWithSoftReferenced() {
+        Map<Integer, SoftReference<BigArray>> bigArrayWeakReferenceMap = new HashMap<>();
+        for (int i = 0; i < 100; i++) {
+            SoftReference<BigArray> bigArrayWeakReference = new SoftReference<>(new BigArray(i));
+            bigArrayWeakReferenceMap.put(i, bigArrayWeakReference);
         }
     }
 
@@ -79,6 +103,7 @@ public class GcExample {
          * 当对象变成(GC Roots)不可达时，GC会判断该对象是否覆盖了finalize方法，若未覆盖，则直接将其回收。否则，若对象未执行过finalize方法，
          * 将其放入F-Queue队列，由一低优先级线程执行该队列中对象的finalize方法。执行finalize方法完毕后，GC会再次判断该对象是否可达，若不可达，则进行回收，否则，对象“复活”。
          * 参考：https://www.cnblogs.com/fnlingnzb-learner/p/7283867.html
+         *
          * @throws Throwable
          */
 //        @Override
@@ -86,5 +111,9 @@ public class GcExample {
 //            // System.out.println(index + " is being finalized by gc");
 //            super.finalize();
 //        }
+        @Override
+        public String toString() {
+            return String.valueOf(index);
+        }
     }
 }
