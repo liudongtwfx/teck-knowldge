8c55b997e247e55114c86daea69e853ec1801288
Liu Dong <liudong17@meituan.com>
2020-03-29 14:15:12 +0800

    [MOD] 调整目录结构

--- src/main/java/jl/gc/GcExample.java
+++ src/main/java/jl/gc/GcExample.java
@@ -4,31 +4,52 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
 
 
-/**
- * -Xms64M
- * -Xmx64M
- * -XX:+PrintGCDetails
- * -verbose:gc
- * -XX:+PrintHeapAtGC
- * -XX:SurvivorRatio=8
- */
 public class GcExample {
+
+    private static final String JVM_ARGS = "-Xms64M\n" +
+            "-Xmx64M\n" +
+            "-XX:+PrintGCDetails\n" +
+            "-verbose:gc\n" +
+            "-XX:+PrintHeapAtGC\n" +
+            "-XX:SurvivorRatio=8\n" +
+            "-XX:+UseConcMarkSweepGC";
+
     public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            //functionWithStack();
+            // List<LargeExample> largeExamples = functionWithReturn();
+        }
+        demoWithWeakRefrence();
+    }
+
+    private static void functionWithStack() {
         List<LargeExample> largeExamplee = new ArrayList<>();
-
-        for (int i = 0; i < 300000; i++) {
+        for (int i = 0; i < 100000; i++) {
             largeExamplee.add(new LargeExample(i, String.valueOf(i)));
-            //System.gc();
         }
+    }
 
-        for (LargeExample largeExample : largeExamplee) {
-            //do nothing
+    private static List<LargeExample> functionWithReturn() {
+        List<LargeExample> largeExample = new ArrayList<>();
+        for (int i = 0; i < 100000; i++) {
+            largeExample.add(new LargeExample(i, String.valueOf(i)));
         }
+        return largeExample;
+    }
 
+    /**
+     *
+     */
+    private static void demoWithWeakRefrence() {
+        for (int i = 0; i < 100; i++) {
+            WeakReference<BigArray> bigArrayWeakReference = new WeakReference<>(new BigArray());
+
+        }
     }
 
 
@@ -38,5 +59,16 @@ public class GcExample {
     private static class LargeExample {
         private Integer a;
         private String name;
+
+        @Override
+        protected void finalize() throws Throwable {
+            super.finalize();
+//            System.out.println(name + " is being finalized by gc");
+        }
+    }
+
+
+    private static class BigArray {
+        private final int[] nums = new int[1024 * 1024 * 10];
     }
 }
--- src/main/java/jl/usage/spi/ServiceLoaderExample.java
+++ src/main/java/jl/usage/spi/ServiceLoaderExample.java
@@ -6,6 +6,6 @@ public class ServiceLoaderExample {
     public static void main(String[] args) throws ClassNotFoundException {
         ServiceLoader<IService> load = ServiceLoader.load(IService.class);
 
-        load.forEach(l -> l.print());
+        load.forEach(IService::print);
     }
 }
