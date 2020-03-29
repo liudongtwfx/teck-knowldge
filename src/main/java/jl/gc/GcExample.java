d531a5248850f5b2eb4f447c56cbec234dbdb870
Liu Dong <liudong17@meituan.com>
2020-03-29 14:56:05 +0800

    [MOD] gc

--- src/main/java/jl/gc/GcExample.java
+++ src/main/java/jl/gc/GcExample.java
@@ -24,7 +24,7 @@ public class GcExample {
             //functionWithStack();
             // List<LargeExample> largeExamples = functionWithReturn();
         }
-        demoWithWeakRefrence();
+        demoWithWeakReferenced();
     }
 
     private static void functionWithStack() {
@@ -43,12 +43,11 @@ public class GcExample {
     }
 
     /**
-     *
+     * 一般在弱引用的同时，这个对象可能也被强引用了。如果这个强引用消失了，系统就开始回收弱引用。
      */
-    private static void demoWithWeakRefrence() {
+    private static void demoWithWeakReferenced() {
         for (int i = 0; i < 100; i++) {
-            WeakReference<BigArray> bigArrayWeakReference = new WeakReference<>(new BigArray());
-
+            WeakReference<BigArray> bigArrayWeakReference = new WeakReference<>(new BigArray(i));
         }
     }
 
@@ -70,5 +69,22 @@ public class GcExample {
 
     private static class BigArray {
         private final int[] nums = new int[1024 * 1024 * 10];
+        private int index;
+
+        BigArray(int index) {
+            this.index = index;
+        }
+
+        /**
+         * 当对象变成(GC Roots)不可达时，GC会判断该对象是否覆盖了finalize方法，若未覆盖，则直接将其回收。否则，若对象未执行过finalize方法，
+         * 将其放入F-Queue队列，由一低优先级线程执行该队列中对象的finalize方法。执行finalize方法完毕后，GC会再次判断该对象是否可达，若不可达，则进行回收，否则，对象“复活”。
+         * 参考：https://www.cnblogs.com/fnlingnzb-learner/p/7283867.html
+         * @throws Throwable
+         */
+//        @Override
+//        protected void finalize() throws Throwable {
+//            // System.out.println(index + " is being finalized by gc");
+//            super.finalize();
+//        }
     }
 }
