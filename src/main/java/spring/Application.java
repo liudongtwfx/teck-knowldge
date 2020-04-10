f07b89bb9127b61e7959fe80d5e4a848c5cbffca
liudong17 <liudong17@meituan.com>
2020-04-10 20:55:34 +0800

    [MOD] 修改日志

--- src/main/java/spring/Application.java
+++ src/main/java/spring/Application.java
@@ -1,16 +1,18 @@
 package spring;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(scanBasePackages = {"spring"})
+@Slf4j
 public class Application {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
         for (String beanDefinitionName : context.getBeanDefinitionNames()) {
-            System.out.println(beanDefinitionName);
+            log.info("{}", beanDefinitionName);
         }
     }
 }
