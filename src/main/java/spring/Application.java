b8c154d8b274e1a5d8866c5e5119a5aa64ba1c4a
Liu Dong <liudong17@meituan.com>
2020-04-13 12:57:30 +0800

    [MOD] 去除无效文件

--- src/main/java/spring/Application.java
+++ src/main/java/spring/Application.java
@@ -1,16 +1,28 @@
 package spring;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
 
 @SpringBootApplication(scanBasePackages = {"spring"})
+@Slf4j
 public class Application {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
         for (String beanDefinitionName : context.getBeanDefinitionNames()) {
-            System.out.println(beanDefinitionName);
+            log.info("{}", beanDefinitionName);
         }
+        Map<String, Long> beansOfType = context.getBeansOfType(Long.class);
+        beansOfType.forEach((k, v) -> log.info("key:{},value:{}", k, v));
+    }
+
+    @Bean
+    public Long timestamp() {
+        return System.currentTimeMillis();
     }
 }
