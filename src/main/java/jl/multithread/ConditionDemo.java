20345d5d1f4a0b214cbcdd41ac23e25bf004b4b0
liudong17 <liudong17@meituan.com>
2020-07-22 12:02:36 +0800

    [MOD] 线程同步方式

---
+++ src/main/java/jl/multithread/ConditionDemo.java
@@ -0,0 +1,65 @@
+package jl.multithread;
+
+import lombok.SneakyThrows;
+
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ConditionDemo {
+    private ReentrantLock lock;
+    private Condition printCondition;
+
+    ConditionDemo() {
+        lock = new ReentrantLock();
+        printCondition = lock.newCondition();
+    }
+
+    public static void main(String[] args) {
+        ConditionDemo demo = new ConditionDemo();
+        demo.printNumberAndChar();
+    }
+
+    void printNumberAndChar() {
+        new Thread(new NumberPrinter()).start();
+        new Thread(new CharPrinter()).start();
+    }
+
+
+    private class NumberPrinter implements Runnable {
+        private int num = 0;
+
+        @SneakyThrows
+        @Override
+        public void run() {
+            while (true) {
+                lock.lock();
+                System.out.println(num++);
+                System.out.println(num++);
+                printCondition.signal();
+                printCondition.await();
+                lock.unlock();
+            }
+        }
+    }
+
+
+    private class CharPrinter implements Runnable {
+        private int num = 0;
+
+        @Override
+        @SneakyThrows
+        public void run() {
+            while (true) {
+                lock.lock();
+                char c = (char) ('a' + num++);
+                System.out.println(c);
+                if (c == 'z') {
+                    System.exit(0);
+                }
+                printCondition.signal();
+                printCondition.await();
+                lock.unlock();
+            }
+        }
+    }
+}
---
+++ src/main/java/jl/multithread/WaitNotifyDemo.java
@@ -0,0 +1,57 @@
+package jl.multithread;
+
+import lombok.SneakyThrows;
+
+public class WaitNotifyDemo {
+    private static final Object NUMBER_LOCK = new Object();
+    private static final Object CHAR_LOCK = new Object();
+
+    /**
+     * 12a34b56e
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        new Thread(new NumberRunner()).start();
+        new Thread(new CharRunner()).start();
+    }
+
+    public static class NumberRunner implements Runnable {
+        private static int num = 0;
+
+        @SneakyThrows
+        @Override
+        public void run() {
+            while (true) {
+                synchronized (CHAR_LOCK) {
+                    // CHAR_LOCK.wait();
+                    System.out.println(num++);
+                    System.out.println(num++);
+                    CHAR_LOCK.notify();
+                    CHAR_LOCK.wait();
+                }
+            }
+        }
+    }
+
+    public static class CharRunner implements Runnable {
+        private static int num = 0;
+
+        @Override
+        @SneakyThrows
+        public void run() {
+            while (true) {
+                synchronized (CHAR_LOCK) {
+                    char c = (char) ('a' + num);
+                    System.out.println(c);
+                    if (c == 'z') {
+                        System.exit(0);
+                    }
+                    num++;
+                    CHAR_LOCK.notify();
+                    CHAR_LOCK.wait();
+                }
+            }
+        }
+    }
+}
