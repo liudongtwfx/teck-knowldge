53f39cffb4b7fe99ed7eefdfe66e900f135daea4
liudong17 <liudong17@meituan.com>
2020-08-07 14:14:00 +0800

    [MOD] 增加部分注释

--- src/main/java/jl/multithread/HashMapNotSafeDemo.java
+++ src/main/java/jl/multithread/HashMapNotSafeDemo.java
@@ -4,27 +4,32 @@ import lombok.SneakyThrows;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 public class HashMapNotSafeDemo {
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(String.valueOf(011));
+        CountDownLatch countDownLatch = new CountDownLatch(100);
         for (int i = 0; i < 100; i++) {
-            new Thread(new MapPutRunner(i * 100, (i + 1) * 100)).start();
+            new Thread(new MapPutRunner(i * 100, (i + 1) * 100, countDownLatch)).start();
         }
 
-        Thread.sleep(3000);
+        countDownLatch.await(3000, TimeUnit.MILLISECONDS);
         System.out.println(MapPutRunner.NUM_MAP.size());
     }
 
 
     private static class MapPutRunner implements Runnable {
-        private static final Map<Count, Integer> NUM_MAP = new HashMap<>();
+        private static final Map<Count, Integer> NUM_MAP = new ConcurrentHashMap<>();
         private final int start;
         private final int end;
+        private final CountDownLatch countDownLatch;
 
-        public MapPutRunner(int start, int end) {
+        public MapPutRunner(int start, int end, CountDownLatch countDownLatch) {
             this.start = start;
             this.end = end;
+            this.countDownLatch = countDownLatch;
         }
 
         @SneakyThrows
@@ -34,6 +39,7 @@ public class HashMapNotSafeDemo {
                 NUM_MAP.put(new Count(i), i);
                 Thread.sleep(1);
             }
+            countDownLatch.countDown();
         }
     }
 
@@ -45,16 +51,16 @@ public class HashMapNotSafeDemo {
         }
 
         @Override
+        public int hashCode() {
+            return 1;
+        }
+
+        @Override
         public boolean equals(Object o) {
             if (this == o) return true;
             if (o == null || getClass() != o.getClass()) return false;
             Count count1 = (Count) o;
             return count == count1.count;
-        }
-
-        @Override
-        public int hashCode() {
-            return 1;
         }
     }
 }
