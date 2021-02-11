88d18864e86d091937290157e17756cf1c3905f2
liudongtwfx <liudongtwfx@126.com>
2021-02-11 11:57:41 +0800

    [MOD] 部分算法

--- knowledge/src/main/java/jl/usage/classloader/MyClassloader.java
+++ knowledge/src/main/java/jl/usage/classloader/MyClassloader.java
@@ -4,6 +4,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
+import java.lang.reflect.InvocationTargetException;
 
 /**
  * 继承ClassLoader类，重写findclass方法。
@@ -18,13 +19,15 @@ public class MyClassloader extends ClassLoader {
     }
 
     public static void main(String[] args)
-            throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+            throws IllegalAccessException, InstantiationException, ClassNotFoundException, NoSuchMethodException, InvocationTargetException {
         MyClassloader classloader = new MyClassloader("./", "myclasscloderz");
         Class<?> c = classloader.loadClass("World");
         System.out.println(c.getClassLoader());
         System.out.println(c.getClassLoader().getParent());
         System.out.println(c.getClassLoader().getParent().getParent());
-        c.newInstance();
+        Object instance = c.newInstance();
+        Object name = c.getMethod("toString").invoke(instance);
+        System.out.println(name);
     }
 
     /**
@@ -36,7 +39,7 @@ public class MyClassloader extends ClassLoader {
         return defineClass(name, b, 0, b.length);
     }
 
-    public byte[] loadClassData(String name) {
+    private byte[] loadClassData(String name) {
         name = path + name + ".class";
 
         try (InputStream in = new FileInputStream(new File(name));
